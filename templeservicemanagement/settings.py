c67b9e7522c3412dc0e058319341b6fb15d69040
Amjad shaneeb <amjadshaneeb783@.com>
2025-04-01 20:30:56 +0530

    Fixed Procfile for Gunicorn

--- templeservicemanagement/settings.py
+++ templeservicemanagement/settings.py
@@ -26,7 +26,12 @@ SECRET_KEY = 'django-insecure-#z81_pa7#wb0iux)ie2iz#s$3_c$0n!_*kinarwyc0&it7wvlu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["44.226.145.213",
+"54.187.200.255",
+"34.213.214.55",
+"35.164.95.156",
+"44.230.95.183",
+"44.229.200.200"]
 
 
 # Application definition
